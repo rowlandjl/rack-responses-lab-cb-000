b10868d53e52343020e147cb455b7ac4eec1b0e3
Jessica Rowland <rowlandjl@vcu.edu>
2019-10-21 16:10:36 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -2,6 +2,10 @@ class Application
   
   def call(env)
     resp = Rack::Response.new 
+    
+    time_1 = Kernel.rand(1..24)
+    time_2 = Kernel.rand(1..24)
+    time_3 = Kernel.rand(1..24)
   end 
   
 end 
